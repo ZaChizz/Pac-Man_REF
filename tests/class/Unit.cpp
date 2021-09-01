050ec8dd9ec13fb5bada31917b5300d5ed394d5a
Ievgen <promptsite.com.ua@gmail.com>
2021-09-01 14:37:07 +0300

    Added test for Unit class

--- tests/class/Unit.cpp
+++ tests/class/Unit.cpp
@@ -1,9 +1,16 @@
 #include "../../hpp/Unit.h"
+#include "../../config.h"
 
 TEST_CASE("Unit POSITIVE test") {
     SECTION("Constructor()") {
         Unit* a;
         CHECK_NOTHROW(a = new Unit('a'));
+        delete(a);
+    }
+
+    SECTION("Constructor() with custom start Point") {
+        Unit* a;
+        CHECK_NOTHROW(a = new Unit('a', (int)PACMAN_DEFAULT_COORD_X, (int)PACMAN_DEFAULT_COORD_Y));
         delete(a);
     }
 }
